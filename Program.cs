2eb0f94456d7601321c2af6f95b68ec194707a59
Egzamin <Egzamin@DESKTOP-3GOO9MB>
2026-09-07 09:17:38 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===Wizytówka===");
+            Console.WriteLine("Imię i nazwisko: Jan Kowalski");
+            Console.WriteLine("Zawód: Technik programista");
+            Console.WriteLine("Miasto: Warszawa");
+        }
+    }
+}
